235e9251a36bf17f129a164f2c8c8314d9d5e423
rounak2407 <rounak24072004@gmail.com>
2025-04-30 12:19:47 +0530

    Update face_register.py

--- src/core/face_register.py
+++ src/core/face_register.py
@@ -3,7 +3,7 @@ import os
 import time
 
 # Define paths
-BASE_DIR = "/absolute/path/to/PresenSee/"
+BASE_DIR = "/absolute/path/to/PresenSee/"    #define your own path
 HAARCASCADE_PATH = os.path.join(
     BASE_DIR, "models", "haarcascade_frontalface_default.xml")
 USER_DATA_PATH = os.path.join(BASE_DIR, "dataset", "users")
